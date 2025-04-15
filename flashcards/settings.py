518aabdf7cc0058ef85e011b808ad5c4ae88b5fb
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-04-15 12:35:05 -0700

    Initialize decks application

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'accounts.apps.AccountsConfig',
+    'decks.apps.DecksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
