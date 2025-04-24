c327d7f8a82c34c5773b7e53535e3b455b5880aa
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-04-23 18:05:04 -0700

    Add description and published fields to Deck model

--- decks/models.py
+++ decks/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Deck(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    description = models.TextField(default="No description")
+    published = models.BooleanField(default=False)
 
     def get_absolute_url(self):
         return reverse('deck-detail', kwargs={ 'pk': self.pk })
