db93a3925e3b001fb16984657ac20ed03d26900c
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-04-25 04:06:42 -0700

    Add link to user who made the deck in the listings

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
-    path("profile/", views.UserProfileView.as_view(), name="profile"),
+    path("profile/", views.UserProfileView.as_view(), name="my-profile"),
+    path("profile/<int:pk>", views.UserProfileView.as_view(), name="user-profile"),
 ]
