5d2f101b17cd81bc1364b395a3f6a814df6e30f3
hasanQ <haslang01@gmail.com>
2025-04-25 07:55:36 -0700

    added tests for accounts and decks

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class SignUpViewTests(TestCase):
+    def setUp(self):
+        self.url = reverse('signup')
+
+    def test_signup_page_loads(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
