79c537051775e7c70ba952291510b796a59b7647
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-04-30 16:00:39 -0700

    Add some tests for access permissions

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,12 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from typing import override
+
 
 class SignUpViewTests(TestCase):
+    @override
     def setUp(self):
-        self.url = reverse('signup')
+        self.url = reverse("signup")
 
     def test_signup_page_loads(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'accounts/signup.html')
+        self.assertTemplateUsed(response, "accounts/signup.html")
