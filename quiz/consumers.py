60a17107db38676a2dfd54968a31ac781c0a8985
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-04-30 20:09:32 -0700

    Register channel group

--- quiz/consumers.py
+++ quiz/consumers.py
@@ -8,12 +8,17 @@ class QuizWebSocketConsumer(AsyncJsonWebsocketConsumer):
     @override
     async def connect(self):
         self.quiz_pk = self.scope["url_route"]["kwargs"]["quiz_pk"]
-        await self.channel_layer.group_add(self.quiz_pk, self.channel_name)
+        self.quiz_channel_group_name = "quiz_{}".format(self.quiz_pk)
+        await self.channel_layer.group_add(
+            self.quiz_channel_group_name, self.channel_name
+        )
         await self.accept()
 
     @override
     async def receive_json(self, content):
         match content:
             case {"action": "start"}:
-                print("starting")
                 await sync_to_async(start_quiz.send)(self.quiz_pk)
+
+    async def quiz_start(self, event):
+        print("Received start signal")
--- quiz/tasks.py
+++ quiz/tasks.py
@@ -1,6 +1,11 @@
+from channels.layers import get_channel_layer
 from dramatiq import actor
+from asgiref.sync import async_to_sync
 
 
 @actor
 def start_quiz(pk):
-    print("Starting quiz with pk {}".format(pk))
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        "quiz_{}".format(pk), {"type": "quiz.start"}
+    )
