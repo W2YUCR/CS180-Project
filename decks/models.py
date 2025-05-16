43a997b0230f989a367729f4b822f9aa929dea5c
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-05-15 21:28:58 -0700

    Add necessary fields

--- decks/models.py
+++ decks/models.py
@@ -1,7 +1,10 @@
-#decks/models.py
+# decks/models.py
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from fsrs_rs_python import FSRS, MemoryState
+
 
 class Deck(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,12 +13,31 @@ class Deck(models.Model):
     published = models.BooleanField(default=False)
 
     def get_absolute_url(self):
-        return reverse('deck-detail', kwargs={ 'pk': self.pk })
+        return reverse("deck-detail", kwargs={"pk": self.pk})
+
 
 class Card(models.Model):
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
     front = models.TextField()
     back = models.TextField()
+    stability = models.FloatField(default=0.0)
+    difficulty = models.FloatField(default=5.0)
+    last_reviewed = models.DateTimeField(null=True)
+    due = models.DateTimeField(null=True)
 
     def get_absolute_url(self):
-        return reverse('card-detail', kwargs={ 'pk': self.pk })
+        return reverse("card-detail", kwargs={"pk": self.pk})
+
+    def review(self):
+        elapsed_days = (datetime.now(datetime.timezone.utc) - self.last_reviewed).days
+        fsrs = FSRS(parameters=DEFAULT_PARAMETERS)
+        memory = MemoryState(self.stability, self.difficulty)
+        next_state = fsrs.next_states(memory, 0.9, elapsed_days)
+        next_state = next_state.good
+        interval = max(1, round(next_state.interval))
+
+        self.stability = next_state.memory.stability
+        self.difficulty = next_state.memory.difficulty
+        self.due = self.last_reviewed + timedelta(days=interval)
+
+        self.save()
