f36d6a509eb5b5838ff409df63db8afc730118f9
hasanQ <haslang01@gmail.com>
2025-06-04 17:04:39 -0700

    reviews tests modified

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,43 +1,42 @@
 from types import MethodType
+from typing import override
+from datetime import timedelta
 
+from asgiref.sync import async_to_sync
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.utils import timezone
-
-from asgiref.sync import async_to_sync
 
 from decks.models import Deck, Card
 from reviews.consumers import ReviewWebSocketConsumer
 
 
 class ReviewConsumerTests(TestCase):
-    def setUp(self):
+    @override
+    def setUp(self) -> None:
         self.user = User.objects.create_user("u", password="p")
-        self.deck = Deck.objects.create(owner=self.user,
-                                        name="demo", description="d")
+        self.deck = Deck.objects.create(owner=self.user, name="demo", description="d")
         self.consumer = ReviewWebSocketConsumer()
         self.consumer.deck = self.deck
 
-
-    def test_get_random_card_returns_due(self):
-        past = timezone.now() - timezone.timedelta(hours=1)
-        due_card = Card.objects.create(deck=self.deck, front="due",
-                                       back="A", due=past)
+    def test_get_random_card_returns_due(self) -> None:
+        past = timezone.now() - timedelta(hours=1)
+        due_card = Card.objects.create(deck=self.deck, front="due", back="A", due=past)
 
         self.consumer.get_random_card()
         self.assertEqual(self.consumer.card, due_card)
 
-    def test_send_next_emits_end_when_empty(self):
-        messages = []
+    def test_send_next_emits_end_when_empty(self) -> None:
+        messages: list[dict[str, str]] = []
 
-        async def fake_send_json(self, payload):
+        async def fake_send_json(self, payload: dict[str, str]) -> None:
             messages.append(payload)
 
-        def fake_get_random_card(self):
+        def fake_get_random_card(self) -> None:
             self.card = None
 
-        self.consumer.send_json = MethodType(fake_send_json, self.consumer)
-        self.consumer.get_random_card = MethodType(fake_get_random_card, self.consumer)
+        self.consumer.send_json = MethodType(fake_send_json, self.consumer)  # type: ignore
+        self.consumer.get_random_card = MethodType(fake_get_random_card, self.consumer)  # type: ignore
 
         async_to_sync(self.consumer.send_next)()
         self.assertEqual(messages[0]["type"], "end")
